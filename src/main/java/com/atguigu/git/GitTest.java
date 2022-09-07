6859b64a84e7f498fae9bad89b38194b73d7d8a3
chung <whwh1997@foxmail.com>
2022-09-07 14:12:14 +0800

    hot-fix commit 1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,5 +3,8 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello github");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
     }
 }
